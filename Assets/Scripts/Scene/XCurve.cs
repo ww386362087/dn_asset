5560d3df3a594a1a0e9b15329e185f80ea1a7e88
huailiang.peng <alexpeng@123u.com>
2017-08-17 16:11:36 +0800

    scene

--- Assets/Scripts/Scene/XCurve.cs
+++ Assets/Scripts/Scene/XCurve.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
 
-public class XCurve : MonoBehaviour 
+public class XCurve : MonoBehaviour
 {
     public float Max_Value = 0;
     public float Land_Value = 0;
     public AnimationCurve Curve = new AnimationCurve();
 
-	public int length { get {return Curve.length;} }
+    public int length { get { return Curve.length; } }
 
-	public float Evaluate(float time)
-	{
-		return Curve.Evaluate (time);
-	}
-	public float GetValue(int index)
-	{
-		return Curve [index].value;
-	}
+    public float Evaluate(float time)
+    {
+        return Curve.Evaluate(time);
+    }
+    public float GetValue(int index)
+    {
+        return Curve[index].value;
+    }
 
-	public float GetTime(int index)
-	{
-		return Curve [index].time;
-	}
+    public float GetTime(int index)
+    {
+        return Curve[index].time;
+    }
 
     public float GetMaxValue()
     {
@@ -31,5 +31,5 @@ public class XCurve : MonoBehaviour
     {
         return Land_Value;
     }
-    
+
 }
