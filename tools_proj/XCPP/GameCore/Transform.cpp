8743586e909fe46f5f110982e3b3891349f28b24
huailiang.peng <alexpeng@123u.com>
2017-12-31 18:02:05 +0800

    native

--- tools_proj/XCPP/GameCore/Transform.cpp
+++ tools_proj/XCPP/GameCore/Transform.cpp
@@ -1,17 +1,13 @@
 #include "Transform.h"
 
 
-Transform::Transform()
-{
-	position = Vector3::zero;
-	rotatiion = Vector3::zero;
-	scale = Vector3::one;
-}
 
 Transform::Transform(const char* nm)
 {
 	name = nm;
-	Transform();
+	position = Vector3::zero;
+	rotatiion = Vector3::zero;
+	scale = Vector3::one;
 }
 
 Transform::~Transform()
--- tools_proj/XCPP/GameCore/Transform.h
+++ tools_proj/XCPP/GameCore/Transform.h
@@ -7,7 +7,6 @@ class  GameObject;
 class Transform
 {
 public:
-	Transform();
 	Transform(const char* nm);
 	~Transform();
 	
